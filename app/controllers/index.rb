7556e16b8af3e16b272aba606f6232dbb6b59498
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-23 20:21:21 -0500

    working on rendering a partial when the user clicks to buy the stock

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -34,5 +34,16 @@ end
 
 get '/company_page/:id' do
   @company = Company.find(params[:id])
+  historical_prices = @company.prices
+  
+  @array_of_dates_and_prices = [['date','price']]
+  historical_prices.each do |object| 
+    @array_of_dates_and_prices << [object.date.to_s, object.price] 
+  end 
+  
   erb :company_page
+end
+
+get '/buy_stock' do 
+  erb :stock_list
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,5 +1,4 @@
 class Trade < ActiveRecord::Base
   belongs_to :shareholder, class_name: "User" # renaming user to shareholder, but making shareholder = user (model)
   belongs_to :company
-
 end
